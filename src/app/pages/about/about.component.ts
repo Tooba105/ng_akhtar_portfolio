d62de875de5a32b364197863c737a2cbebecec77
MUHAMMAD AKHTAR <makhtarkhan581@outlook.com>
2025-04-08 18:27:49 +0500

    added cv and updated about

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-about',
@@ -13,10 +15,21 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 export class AboutComponent {
   showEducation: boolean = true;
   showProjects: boolean = true;
+  private router = inject(Router);
 
   projects = [
     { name: 'Project 1', description: 'Description of Project 1' },
     { name: 'Project 2', description: 'Description of Project 2' },
     { name: 'Project 3', description: 'Description of Project 3' },
   ];
+
+  downloadCV() {
+    const link = document.createElement('a');
+    link.href = 'assets/cv.pdf';
+    link.download = 'Muhammad-Akhtar-CV.pdf'; // customize filename as needed
+    link.click();
+  }
+  goToContact() {
+    this.router.navigate(['/contact']);
+  }
 }
